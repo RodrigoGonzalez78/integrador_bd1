dc57c8584b36b1a3cd115fc2cd5efbe305967966
RodrigoGonzalez78 <61030017+RodrigoGonzalez78@users.noreply.github.com>
2023-11-19 18:41:49 -0300

    backupYRestore cambios 1

--- backupYRestore/implementacion.sql
+++ backupYRestore/implementacion.sql
@@ -1,8 +1,8 @@
 
-
--- 1 Verificar el modo de recuperacion de la base de datos
+-- 1 Verificar el modo de recuperación de la base de datos
 use base_consorcio
 
+-- Selecciona el nombre y el modo de recuperación de la base de datos 'base_consorcio'
 SELECT name, recovery_model_desc
 FROM sys.databases
 WHERE name = 'base_consorcio';
@@ -11,18 +11,19 @@ WHERE name = 'base_consorcio';
 -- 2 cambiamos el modo de recuperacion
 
 USE master; -- Asegúrate de estar en el contexto de la base de datos master
+
+-- Cambia el modo de recuperación de 'base_consorcio' a 'FULL'
 ALTER DATABASE base_consorcio
 SET RECOVERY FULL;
 
 -- 3 Realizamos backup de la base de datos
 
+-- Realiza un respaldo completo de la base de datos 'base_consorcio' en el directorio especificado
 BACKUP DATABASE base_consorcio
 TO DISK = 'C:\backup\consorcio_backup.bak'
 WITH FORMAT, INIT;
 
--- Agregamos 10 registros
-
-select * from gasto;
+-- Agregamos 10 registros a la tabla 'gasto'
 
 insert into gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe) 
 values (1,1,1,5,GETDATE(),5,1200);
@@ -99,6 +100,7 @@ values (13,10,2,1,GETDATE(),1,1100);
 
 --5 Realizamos backup del log en otra ubicacion
 
+-- Realiza un respaldo del log de 'base_consorcio' en una ubicación diferente
 BACKUP LOG base_consorcio
 TO DISK = 'C:\backup\logs\LogBackup2.trn'
 WITH FORMAT, INIT;
@@ -107,117 +109,27 @@ WITH FORMAT, INIT;
 
 use master
 
+-- Restaura la base de datos 'base_consorcio' desde el respaldo completo con REPLACE y NORECOVERY
 RESTORE DATABASE base_consorcio
 FROM DISK = 'C:\backup\consorcio_backup.bak'
 WITH REPLACE, NORECOVERY;
 
+-- Restaura el primer log con RECOVERY para aplicar los registros y dejar la base de datos en un estado utilizable
 RESTORE LOG base_consorcio
 FROM DISK = 'C:\backup\LogBackup.trn'
 WITH RECOVERY;
 
 -- Segundo log
 
+-- Restaura el segundo log con RECOVERY para aplicar los registros adicionales y dejar la base de datos en un estado utilizable
 RESTORE LOG base_consorcio
 FROM DISK = 'C:\backup\logs\LogBackup2.trn'
 WITH RECOVERY;
 
+-- Consultamos la tabla 'gasto' después de las restauraciones
+
 select * from gasto
 
-
-
------ Implementacion de Transacciones y Transacciones Anidadas
-
-	
-
-
-
-
-
-
-
------ Implementacion de Indices Columnares en SQL Server 
-
-
-
---- Creacion de la tabla.
-Create table gastoNew	(
-						idgastoNew int identity,
-						idprovincia int,
-                         idlocalidad int,
-                         idconsorcio int, 
-					     periodo int,
-					     fechapago datetime,					     
-						 idtipogasto int,
-						 importe decimal (8,2),	
-					     Constraint PK_gastoNew PRIMARY KEY (idgastoNew),
-						 Constraint FK_gastoNew_consorcio FOREIGN KEY (idprovincia,idlocalidad,idconsorcio)  REFERENCES consorcio(idprovincia,idlocalidad,idconsorcio),
-						 Constraint FK_gastoNew_tipo FOREIGN KEY (idtipogasto)  REFERENCES tipogasto(idtipogasto)					     					     						 					     					     
-							)
-go
-
-
---select * from gasto;
-
-
---CARGA DE UN MILLON DE REGISTROS
---INSERCION POR LOTES
-
--- select * from gastoNew;
-
-declare @tamanioLote int = 1000; -- Tamaño del lote
-declare @cont int = 1;     -- Contador de lotes
-
--- Inicia un bucle para la inserción por lotes
-while @cont <= 1000000 -- Cambia 1000 por el número de lotes necesarios para un millón de registros
-begin
-    insert into gastoNew (idprovincia, idlocalidad, idconsorcio, periodo, fechapago, idtipogasto, importe)
-    select top (@tamanioLote) idprovincia, idlocalidad, idconsorcio, periodo, fechapago, idtipogasto, importe
-    from gasto
-    where idgasto not in (select idgastoNew from gastoNew); -- Evita duplicados
-
-    set @cont = @cont + 1;
-end;
-
-
-
--- Declarar el tamaño del lote y el contador
-DECLARE @BatchSize INT = 1000; -- Tamaño del lote
-DECLARE @Counter INT = 1;     -- Contador de lotes
-
--- Iniciar un bucle para la inserción por lotes
-WHILE @Counter <= 1000000 -- Cambia 1000 al número de lotes necesarios para un millón de registros
-BEGIN
-    -- Insertar registros desde gasto a gastoNew en lotes
-    INSERT INTO gastoNew (idprovincia, idlocalidad, idconsorcio, periodo, fechapago, idtipogasto, importe)
-    SELECT TOP (@BatchSize) idprovincia, idlocalidad, idconsorcio, periodo, fechapago, idtipogasto, importe
-    FROM gasto
-    WHERE idgasto NOT IN (SELECT idgasto FROM gastoNew); -- Evitar duplicados
-
-    SET @Counter = @Counter + 1;
-END;
-
-insert into gastoNew (idprovincia, idlocalidad, idconsorcio, periodo, fechapago, idtipogasto, importe)
-select idprovincia, idlocalidad, idconsorcio, periodo, fechapago, idtipogasto, importe
-from gasto
-order by idgasto
-offset 0 rows
-fetch next 1000000 rows only; -- Ajusta el tamaño del lote según tus necesidades
-
-
-
-
-INSERT INTO gastoNew (idprovincia, idlocalidad, idconsorcio, periodo, fechapago, idtipogasto, importe)
-SELECT idprovincia, idlocalidad, idconsorcio, periodo, fechapago, idtipogasto, importe
-FROM (
-    SELECT idprovincia, idlocalidad, idconsorcio, periodo, fechapago, idtipogasto, importe,
-           ROW_NUMBER() OVER (ORDER BY idgasto) AS RowNumber
-    FROM gasto
-) AS Subquery
-WHERE RowNumber BETWEEN 1 AND 1000000;
-
-
-
---- Pruebas 
 
 
 
